92a8f98a124d5a596561af2a323d9b4e2b408c31
Esteban <esteban@fwd.dev>
2026-09-13 18:57:08 +0000

    chore: bump NanoPay version

--- pay.js
+++ pay.js
@@ -1,4 +1,4 @@
-// NanoPay 2.0.9
+// NanoPay 2.0.10
 // November 1, 2025
 // Released under MIT License
 // (c) @Nano2dev <support@nano.to>
@@ -351,7 +351,7 @@ const SecurityUtils = {
 	window.check_interval = false
 	window.expiration_interval = false
 
-	if (window.NanoPay === undefined) window.NanoPay = { version: '2.0.9' }
+	if (window.NanoPay === undefined) window.NanoPay = { version: '2.0.10' }
 
 	if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
 		window.NanoPay.dark_mode = true
